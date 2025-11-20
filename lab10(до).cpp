3b487f5d68e6baea3e04bf7d071b61e37250f098
Wведэшка <f.fa.66@mail.ru>
2025-11-20 10:03:46 +0000

    готово

--- lab10(до).cpp
+++ lab10(до).cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+template <typename Arg>
+Arg product(Arg arg) { return arg; }
+template <typename First, typename... Other>
+auto product(First first, Other... other) {
+    return first * product(other...);}
+
+int main() {
+    auto result1 = product(2, 3, 4);        
+    cout<<result1<<endl;
+    auto result2 = product(1.5, 2.1, 3.0);  
+    cout<<result2<<endl;
+    auto result3 = product(5);               
+    cout<<result3<<endl;
+     auto result4 = product(1, 2, 3, 4, 5, 6);
+    cout<<result4;
+    return 0;}
